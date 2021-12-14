97f396778828b2e6ac1728a2bc3873da2befe15a
RicardoSaldarriagaPayco <ricardo.saldarriaga@payco.co>
2021-12-14 10:47:22 -0500

    fix: se corrige asento me descripcion de carga de metodo de pago

--- epayco-subscription-epayco.php
+++ epayco-subscription-epayco.php
@@ -469,7 +469,7 @@
                               <div class="loading"></div>
                     </div>
                         <p style="text-align: center;" class="epayco-title" id="epayco_title">
-                            <span class="animated-points">Cargando metodos de pago</span>
+                            <span class="animated-points">Cargando mètodos de pago</span>
                             <br>
                             <small class="epayco-subtitle"> Si no se cargan automáticamente, de clic en el botón "Pagar con ePayco"</small>
                         </p> 
