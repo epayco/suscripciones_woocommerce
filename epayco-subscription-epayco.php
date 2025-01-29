2f529e65a0bccb9c2bf37e64570e6d64db65a823
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-01-28 22:33:16 -0500

    Se ajusta boton del checkout cuando la vista se realiza desde un dispositivo movil

--- epayco-subscription-epayco.php
+++ epayco-subscription-epayco.php
@@ -369,7 +369,7 @@ class WC_Payment_Epayco_Subscription extends WC_Payment_Gateway
          <button class="action-oneclick cancel-oneclick" style="background-color: #D8D8D8">Cancelar</button>
          <button class="action-oneclick save-oneclik">Guardar</button>
        </div>
-       <button id="continue-tdc" class="continue-container text-center btnpay" style="background-color: #28303d;" type="submit">
+       <button id="continue-tdc" class="continue-container text-center btnpay" style="background-color: #3582b7;" type="submit">
         Pagar
          <svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> -->
        </button>
